1d2d6243baea3e563df3d350c612e016c80925e1
xzaelot <45275533+xzaelot@users.noreply.github.com>
2022-07-15 10:06:25 +0400

    Add files via upload

---
+++ Coins/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+class coin
+{
+
+    static int[] tetri = { 1, 5, 10, 20, 50 };
+    static int n = tetri.Length;
+
+    static void findMin(int V)
+    {
+        List<int> ans = new List<int>();
+        for (int i = n - 1; i >= 0; i--)
+        {
+
+            while (V >= tetri[i])
+            {
+                V -= tetri[i];
+                ans.Add(tetri[i]);
+            }
+        }
+
+        for (int i = 0; i < ans.Count; i++)
+        {
+            Console.Write(" " + ans[i]);
+        }
+    }
+
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Enter the amount of ");
+        int n =int.Parse(Console.ReadLine());
+        Console.Write("Following is minimal number " +
+                      "of change for " + n + ": ");
+
+        findMin(n);
+    }
+}
